af470b3b0018ae2c3110ba915a7402d748ca71b9
Gabriel Rufino <contato@gabrielrufino.com>
2022-03-29 20:07:55 -0300

    Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+import { version } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentation = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Chess API')
+      .setDescription('Description here')
+      .setVersion(version)
+      .build(),
+  );
+  SwaggerModule.setup('api', app, documentation);
+
   await app.listen(3000);
 }
 bootstrap();
