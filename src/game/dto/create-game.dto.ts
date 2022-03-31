f8a64223df4593e5d6d17afadeda630d92b33a22
Gabriel Rufino <contato@gabrielrufino.com>
2022-03-31 19:24:51 -0300

    IsISO8601 => IsDateString

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsISO8601 } from 'class-validator';
+import { IsDateString } from 'class-validator';
 
 export class CreateGameDto {
   @ApiProperty({ example: '2022-03-29 20:00:00' })
-  @IsISO8601()
+  @IsDateString()
   startsAt: string;
 
   @ApiProperty({ example: '2022-03-29 20:10:00' })
-  @IsISO8601()
+  @IsDateString()
   endsAt: string;
 }
