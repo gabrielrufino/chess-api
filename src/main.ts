52e12fb586967a8e79b3c8ffe3085a6fd283ae0b
Gabriel Rufino <contato@gabrielrufino.com>
2022-04-02 12:23:41 -0300

    Using helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import helmet from 'helmet';
 
+import { AppModule } from './app.module';
 import { version } from '../package.json';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   const documentation = SwaggerModule.createDocument(
     app,
