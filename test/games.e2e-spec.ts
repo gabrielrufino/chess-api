a50e0a9ca18a1a9685ab679e8ee662bbe11a9406
Gabriel Rufino <contato@gabrielrufino.com>
2022-04-12 18:58:23 -0300

    Tests for creating game

--- test/games.e2e-spec.ts
+++ test/games.e2e-spec.ts
@@ -1,9 +1,4 @@
-import {
-  HttpCode,
-  HttpStatus,
-  INestApplication,
-  ValidationPipe,
-} from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import faker from '@faker-js/faker';
@@ -14,9 +9,7 @@ import { GameModule } from '../src/game/game.module';
 import { Player } from '../src/player/entities/player.entity';
 import { PlayerModule } from '../src/player/player.module';
 import { CreateGameDto } from 'src/game/dto/create-game.dto';
-import { CreatePlayerDto } from 'src/player/dto/create-player.dto';
 import { createPlayer } from './helpers/create-player';
-import { create } from 'domain';
 
 describe('GameModule (e2e)', () => {
   let app: INestApplication;
@@ -68,25 +61,6 @@ describe('GameModule (e2e)', () => {
         .expect(HttpStatus.CREATED);
     });
 
-    it('Should return an error when the black player does not exists', async () => {
-      const { id: whitePlayerId } = await createPlayer(app);
-      const blackPlayerId = faker.datatype.number();
-
-      return request(app.getHttpServer())
-        .post('/games')
-        .send({
-          startsAt: '2022-04-03 17:00:00',
-          endsAt: '2022-04-03 17:15:00',
-          whitePlayerId,
-          blackPlayerId,
-        } as CreateGameDto)
-        .expect(HttpStatus.NOT_FOUND)
-        .expect({
-          statusCode: 404,
-          message: 'Player not found',
-        });
-    });
-
     it('Should return an error when does not receive the startsAt', async () => {
       const [{ id: whitePlayerId }, { id: blackPlayerId }] = await Promise.all([
         createPlayer(app),
@@ -132,6 +106,82 @@ describe('GameModule (e2e)', () => {
             'endsAt must be a valid ISO 8601 date string',
           ],
           error: 'Bad Request',
+        });
+    });
+
+    it('Should return an not found error when the black player does not exists', async () => {
+      const { id: whitePlayerId } = await createPlayer(app);
+      const blackPlayerId = faker.datatype.number();
+
+      return request(app.getHttpServer())
+        .post('/games')
+        .send({
+          startsAt: '2022-04-03 17:00:00',
+          endsAt: '2022-04-03 17:15:00',
+          whitePlayerId,
+          blackPlayerId,
+        } as CreateGameDto)
+        .expect(HttpStatus.NOT_FOUND)
+        .expect({
+          statusCode: 404,
+          message: 'Player not found',
+        });
+    });
+
+    it('Should return an not found error when the white player does not exists', async () => {
+      const whitePlayerId = faker.datatype.number();
+      const { id: blackPlayerId } = await createPlayer(app);
+
+      return request(app.getHttpServer())
+        .post('/games')
+        .send({
+          startsAt: '2022-04-03 17:00:00',
+          endsAt: '2022-04-03 17:15:00',
+          whitePlayerId,
+          blackPlayerId,
+        } as CreateGameDto)
+        .expect(HttpStatus.NOT_FOUND)
+        .expect({
+          statusCode: 404,
+          message: 'Player not found',
+        });
+    });
+
+    it("Should return an not found error when both players don't exists", async () => {
+      const whitePlayerId = faker.datatype.number();
+      const blackPlayerId = faker.datatype.number();
+
+      return request(app.getHttpServer())
+        .post('/games')
+        .send({
+          startsAt: '2022-04-03 17:00:00',
+          endsAt: '2022-04-03 17:15:00',
+          whitePlayerId,
+          blackPlayerId,
+        } as CreateGameDto)
+        .expect(HttpStatus.NOT_FOUND)
+        .expect({
+          statusCode: 404,
+          message: 'Player not found',
+        });
+    });
+
+    it('Should return an error when the white player and the black player are the same player', async () => {
+      const { id: whitePlayerId } = await createPlayer(app);
+      const blackPlayerId = whitePlayerId;
+
+      return request(app.getHttpServer())
+        .post('/games')
+        .send({
+          startsAt: '2022-04-03 17:00:00',
+          endsAt: '2022-04-03 17:15:00',
+          whitePlayerId,
+          blackPlayerId,
+        } as CreateGameDto)
+        .expect(HttpStatus.PRECONDITION_FAILED)
+        .expect({
+          statusCode: 412,
+          message: "You can't play against yourself",
         });
     });
   });
